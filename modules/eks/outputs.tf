a8fd9ff8fb7087b274fe21a5aeb20f75f770dcc1
mohamed55979 <72396071+mohamed55979@users.noreply.github.com>
2025-11-14 08:53:45 +0200

    Update outputs.tf

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -32,51 +32,24 @@ output "cluster_version" {
   value       = aws_eks_cluster.main.version
 }
 
-output "node_group_id" {
-  description = "EKS node group IDs"
-  value       = aws_eks_node_group.main[*].id
-}
-
-output "node_group_status" {
-  description = "Status of the EKS node groups"
-  value       = aws_eks_node_group.main[*].status
-}
-
 output "cluster_arn" {
   description = "ARN of the EKS cluster"
   value       = aws_eks_cluster.main.arn
 }
 
-# modules/eks/outputs.tf
-
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane"
-  value       = aws_eks_cluster.main.endpoint
+output "node_group_id" {
+  description = "EKS node group ID"
+  value       = aws_eks_node_group.main.id
 }
 
-output "cluster_certificate_authority_data" {
-  description = "Base64 encoded certificate data required to communicate with the cluster"
-  value       = aws_eks_cluster.main.certificate_authority[0].data
+output "node_group_arn" {
+  description = "ARN of the EKS node group"
+  value       = aws_eks_node_group.main.arn
 }
 
-output "cluster_name" {
-  description = "The name of the EKS cluster"
-  value       = aws_eks_cluster.main.name
-}
-
-output "cluster_id" {
-  description = "The ID of the EKS cluster"
-  value       = aws_eks_cluster.main.id
-}
-
-output "cluster_arn" {
-  description = "The Amazon Resource Name (ARN) of the cluster"
-  value       = aws_eks_cluster.main.arn
-}
-
-output "cluster_security_group_id" {
-  description = "Security group ID attached to the EKS cluster"
-  value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+output "node_group_status" {
+  description = "Status of the EKS node group"
+  value       = aws_eks_node_group.main.status
 }
 
 output "oidc_provider_arn" {
